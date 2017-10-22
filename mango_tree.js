a06d71352163cf3534476fc8825623431878e7fb
agungpangestu <agung_pangestu83@ymail.com>
2017-10-22 22:45:06 +0700

    belom kelar

--- mango_tree.js
+++ mango_tree.js
@@ -6,16 +6,26 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
+    this._Age = 0
+    this._Height = 0
+    this._max_Buah = 0
+    this._kumpulan_buah = []
+    this._jumlah_Buah = 0
+    this._healty = true
+    this._harvest = 0
   }
 
   getAge() {
+    return this._Age
   }
   getHeight() {
-
+ return this._Height
   }
   getFruits() {
+    return this._kumpulan_buah
   }
   getHealtyStatus() {
+    return this._healty
   }
 
 
@@ -23,14 +33,35 @@ class MangoTree {
 
   // Grow the tree
   grow() {
+    this._Age++
+    this._Height += Math.random()*(300 - 200)+ 200
+    if(this._Age>=20){
+      // this._Age += 0
+      this._healty = false
+    }
+
+
   }
 
   // Produce some mangoes
   produceMangoes() {
+    this._jumlah_Buah = Math.floor( Math.random()*11);
+    for (let i = 0;i<this._jumlah_Buah;i++){
+      const mango = new Mango()
+      this._kumpulan_buah.push(mango.prod)
+    }
   }
 
   // Get some fruits
   harvest() {
+    let total = this._jumlah_Buah;
+    let good = 0;
+    let bad = 0
+    for (let i = 0;i<this._kumpulan_buah;i++){
+      console.log(this._kumpulan_buah[i])
+    }
+
+    return this._kumpulan_buah;
   }
 
 }
@@ -38,9 +69,24 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor() {
+    this._petik = this.petik
+  }
+  get petik(){
+    let prod = ['good','bad']
+    let random = Math.floor(Math.random()*2)
+    return {manggo: prod[random]}
   }
 }
 
+let mangoTree = new MangoTree()
+// console.log(mangoTree.produceMangoes());
+// console.log(mangoTree.harvest());
+ do {
+   mangoTree.grow();
+   mangoTree.produceMangoes();
+  //  mangoTree.harverst();
+   console.log(`[Year ${mangoTree._Age} Report] Height = ${mangoTree._Height} | Fruits harvested = ${mangoTree._harvested}`)
+ } while (mangoTree._healty != false)
 /**
   * driver code untuk release 0
   * let mangoTree = new MangoTree()
@@ -53,12 +99,12 @@ class Mango {
   */
 
 // Release 1
-class AppleTree {}
-class Apple {}
-
-// Release 2
-class FruitTree {}
-class Fruit {}
-
-// Release 3
-class TreeGrove {}
+// class AppleTree {}
+// class Apple {}
+//
+// // Release 2
+// class FruitTree {}
+// class Fruit {}
+//
+// // Release 3
+// class TreeGrove {}
