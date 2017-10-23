cc5bc564983bfb50d96c321c6b09b9844abb48f9
aditReza <lete.note3@gmail.com>
2017-10-23 22:04:43 +0700

    release 1

--- mango_tree.js
+++ mango_tree.js
@@ -7,29 +7,15 @@ class MangoTree {
   constructor() {
     this._age            = 0
     this._height         = 0
-    this.capacity_mango  = 0
     this.fruit_available = []
     this._harvested      = 0
     this.healthyStatus   = true
     this.maximum_age     = 19
   }
 
-  getAge() {
-    return this._age
-  }
-  getHeight() {
-    return this._height
-  }
-  getFruits() {
-    return this.fruit_available
-  }
-  getHealtyStatus() {
-    return this.healthyStatus
-  }
-
   // Get current states here
   heigthRandom(){
-    let grow_height = Math.floor(Math.random() * 3 - 1) + 1
+    let grow_height = Math.floor(Math.random() * 6 - 1) + 1
     // console.log(grow_height)
     return grow_height
   }
@@ -49,7 +35,7 @@ class MangoTree {
     // looping push class Mango
     // push ke this.fruit_available = []
     this.fruit_available = []
-    let productive_mango_peryear = Math.floor(Math.random() * 13 - 1) + 1
+    let productive_mango_peryear = Math.floor(Math.random() * 33 - 1) + 1
     for(let i=0; i<productive_mango_peryear; i++){
       this.fruit_available.push(new Mango())
     }
@@ -98,13 +84,7 @@ class Mango {
 
 // driver code untuk release 0
    let mangoTree = new MangoTree()
-   let mango = new Mango()
-   mango.getQuality()
-
-
-   mangoTree.heigthRandom()
-   mangoTree.produceMangoes()
-
+   console.log('\n============================ MANGGO TREE REPORT =======================\n')
    console.log(`The tree is alive! :smile:`)
    do {
      mangoTree.grow();
@@ -116,8 +96,95 @@ class Mango {
 
 
 // Release 1
-class AppleTree {}
-class Apple {}
+class AppleTree {
+  constructor() {
+    this._age            = 0
+    this._height         = 1
+    this.fruit_available = []
+    this._harvested      = 0
+    this.healthyStatus   = true
+    this.maximum_age     = 11
+  }
+  // Get current states here
+  heigthRandom(){
+    let grow_height = Math.floor(Math.random() * 3 - 1) + 1
+    // console.log(grow_height)
+    return grow_height
+  }
+  // Grow the tree
+  grow() {
+    this._age += 1
+    if(this._age <= this.maximum_age){
+      this._height += this.heigthRandom()
+    }else if(this._age >= this.maximum_age){
+      this.healthyStatus = false
+    }
+  }
+
+  // Produce some mangoes
+  produceMangoes() {
+    // random jumlah buah per tahun
+    // looping push class Mango
+    // push ke this.fruit_available = []
+    this.fruit_available = []
+    let productive_mango_peryear = Math.floor(Math.random() * 13 - 1) + 1
+    for(let i=0; i<productive_mango_peryear; i++){
+      this.fruit_available.push(new Mango())
+    }
+    // console.log(this.fruit_available)
+    return this.fruit_available
+
+  }
+
+  // Get some fruits
+  harvest() {
+    let goodQuality = 0
+    let badQuality  = 0
+
+    for(let i=0; i<this.fruit_available.length; i++){
+      // console.log('<><>><<>', this.fruit_available[i].quality);
+      if(this.fruit_available[i].quality == 'good'){
+        goodQuality++
+      }else{
+        badQuality++
+      }
+
+    }
+    // console.log('>>>>>>', goodQuality);
+    return `${goodQuality + badQuality} (${goodQuality} good, ${badQuality} bad)`
+  }
+}
+
+class Apple {
+  constructor() {
+    this.quality = this.getQuality()
+  }
+
+  getQuality(){
+    let randQuality = Math.round(Math.random())
+
+    if(randQuality == 1){
+      this.quality = 'good'
+    }else{
+      this.quality = 'bad'
+    }
+    return this.quality
+  }
+}
+
+// driver code untuk release 0
+   let appleTree = new AppleTree()
+   console.log('\n============================ APPLE TREE REPORT =======================\n')
+   console.log(`The tree is alive! :smile:`)
+   do {
+     appleTree.grow();
+     appleTree.produceMangoes();
+     appleTree.harvest();
+     console.log(`[Year ${appleTree._age} Report] Height = ${appleTree._height} m | Fruits harvested = ${appleTree.harvest()}`)
+   } while (appleTree.healthyStatus != false)
+   console.log(`The tree has met its end. :sad:`)
+
+
 
 // Release 2
 class FruitTree {}
