f50c8825371dc1df66af7861df5498711dd3ab39
rezaalfarabi <anangreza18@gmail.com>
2017-10-23 22:37:02 +0000

    release 0

--- mango_tree.js
+++ mango_tree.js
@@ -5,17 +5,28 @@
 class MangoTree {
 
   // Initialize a new MangoTree
-  constructor() {
-  }
+  constructor(param) {
+    this._age = 0;
+    this._height = 0;
+    this.fruits = [];
+    this.healthy = true;
+
+    }
 
   getAge() {
+    return this._age;
   }
-  getHeight() {
 
+  getHeight() {
+    return this._height;
   }
+
   getFruits() {
+    return this._fruits;
   }
+
   getHealtyStatus() {
+    return this._healthy;
   }
 
 
@@ -23,19 +34,49 @@ class MangoTree {
 
   // Grow the tree
   grow() {
+    this._age++;
+    if (this._age < 10) {
+      this._height += Math.round(random() / 12);
+      return this._healthy = true;
+    } else {
+      return this._healthy = false;
+    }
   }
+
 
   // Produce some mangoes
   produceMangoes() {
+    let buah = random() + 20;
+    let jumlahBuah = (buah > this._maxFruits) ? this._maxFruits : buah;
+
+    for (let i = 0; i < jumlahBuah; i++) {
+      let fruit = new Fruit();
+
+      if (fruit.quality == "good") {
+        this._good++;
+      } else {
+        this._bad++;
+      }
+
+      this._fruits.push(fruit);
+    }
   }
 
   // Get some fruits
+  
   harvest() {
+    this._harvested = `${this._fruits.length} (${this._good} good, ${this._bad} bad)`;
+    this._fruits = [];
+    this._bad = 0;
+    this._good = 0
   }
 
 }
 
 class Mango {
+  constructor() {
+    this.quality = (random() % 2 == 0) ? "good" : "bad";
+  }
   // Produce a mango
   constructor() {
   }
@@ -62,3 +103,32 @@ class Fruit {}
 
 // Release 3
 class TreeGrove {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
