6760ec1eb052a5a84ea3c3c3b30e3f84b07f7b10
Ahmad Fajar & Fickry <pair+ahmad-fajar.biliman@devbootcamp.com>
2017-10-23 20:02:06 +0700

    release 0

--- mango_tree.js
+++ mango_tree.js
@@ -1,93 +1,17 @@
 "use strict"
 
 // release 0
-
+const FruitTree = require('./fruit_tree')
 class MangoTree {
-
-  // Initialize a new MangoTree
-  constructor() {
-    this._age = 0
-    this._height = 0
-    this.healthyStatus = true
-    this._fruits = 0
-    this._harvested = ''
-
+  constructor(produce_per_year, height_per_year, stop_growing, died ){
+    super(produce_per_year, height_per_year, stop_growing, died)
   }
 
-  getAge() {
-    return this._age
-  }
-  getHeight() {
-    return this._height
-  }
-  getFruits() {
-    return this._fruits
-  }
-  getHealtyStatus() {
-    return this.healthyStatus
-  }
-  getFruits(){
-    return this._fruits
-  }
-
-  setAge(num) {
-     this._age+=num
-  }
-
-  setHeight(num){
-    this._height += num
-  }
-
-  setHealthyStatus(str){
-    this.healthyStatus = str
-  }
-
-  setFruits(num){
-    this._fruits = num
-  }
-
-  // Get current states here
-
-  // Grow the tree
-  grow() {
-    this.setAge(1)
-    if(this.getAge() < 20){
-      if(Math.floor(this.getHeight()) <= 5){
-        this.setHeight(Math.random()*1)
-      }
-    }else{
-      this.setHealthyStatus(false)
-    }
-  }
-
-  // Produce some mangoes
-  produceMangoes() {
-    let jumlah = Math.ceil(Math.random() * 20)
-    this.setFruits(jumlah)
-  }
-
-  // Get some fruits
-  harverst() {
-
-    let good = Math.floor(Math.random() * this.getFruits())
-    let bad = this.getFruits() - good
-    // console.log(good, bad, jumlah);
-    this._harvested = new Mango(good, bad)
-  }
-
-}
-
-class Mango {
-  // Produce a mango
-  constructor(good, bad) {
-    this.quality_good = good
-    this.quality_bad = bad
-  }
 }
 
 
 // driver code untuk release 0
-  let mangoTree = new MangoTree()
+  let mangoTree = new MangoTree(20, 2, 8, 10)
   do {
     mangoTree.grow();
      mangoTree.produceMangoes();
@@ -109,10 +33,4 @@ class Mango {
 // // Release 3
 // class TreeGrove {}
 
-// let mangoTree = new MangoTree()
-// // do {
-// //   mangoTree.grow();
-// //    console.log(`[Year ${mangoTree._age} Report] Height = ${mangoTree._height} m | Healthy Status = ${mangoTree.healthyStatus}`)
-// //  } while (mangoTree.healthyStatus != false)
-// mangoTree.produceMangoes()
-//  console.log(mangoTree.harvest());
+
