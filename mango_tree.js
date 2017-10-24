5e777ea62ea72a8feed4ae3514564e8983657177
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-10-24 23:39:11 +0700

    MANGO

--- mango_tree.js
+++ mango_tree.js
@@ -1,17 +1,16 @@
 "use strict"
 
-// release 0
-
 class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
     this.umur = 0;
     this.tinggi = 0;
-    this.max = 0;
-    this.kumpulanBuah = 0;
+    this.max = 30;
+    this.kumpulanBuah = [];
     this.buahPetik = 0;
     this.healthy = true;
+    this.maxAge = 20;
   }
 
   getAge() {
@@ -27,44 +26,56 @@ class MangoTree {
     return this.healthy;
   }
 
-
-  // Get current states here
-
   // Grow the tree
   grow() {
-    debugger;
-      if(this.umur === 20) {
-        this.healthy = false;
-      } else {
-        this.umur = this.umur + 1;
-      }
-
-      if(this.tinggi < 5) {
-        this.tinggi = this.tinggi + 1;
-      }
+    if(this.umur < this.maxAge && this.healthy == true) {
+      this.umur = this.umur + 1;
+      this.tinggi = this.tinggi + 2;
+    } else {
+      this.healthy = false;
+    }
   }
 
   // Produce some mangoes
   produceMangoes() {
-    let angka = '123456789';
-    let random = Math.ceil(Math.random() * angka.length -1);
-
-    this.kumpulanBuah = this.kumpulanBuah + Number(angka[random]);
+    let random = Math.ceil(Math.random() * this.max);
+      // console.log(random);
+      for(let i = 0 ; i < random ; i++) {
+        this.kumpulanBuah.push(new Mango());
+      }
+      return this.kumpulanBuah;
   }
 
-  // Get some fruits
-  harvest() {
 
+  harvest() {
+    let hitungbaik = 0;
+    let hitungburuk = 0;
+      for(let i = 0 ; i < this.kumpulanBuah.length; i++) {
+        if(this.kumpulanBuah[i].kualitas === 'GOOD') {
+          hitungbaik = hitungbaik + 1;
+        } else {
+          hitungburuk = hitungburuk + 1
+        }
+      }
+      return 'Jumlah Buah ' + this.kumpulanBuah.length + ' || ' + 'good : ' + hitungbaik + ' bad ' + hitungburuk;
   }
 
 }
 
 
 class Mango {
-
-  constructor(quality) {
-    this.kualitas = quality;
+  constructor() {
+    this.kualitas = this.getKualitas();
   }
+
+  getKualitas() {
+    let random = Math.round(Math.random() * 3);
+      if(random === 0) {
+        return 'BAD'
+      } else {
+        return 'GOOD'
+      }
+    }
 }
 
 
@@ -73,8 +84,8 @@ class Mango {
  do {
    mangoTree.grow();
    mangoTree.produceMangoes();
-  //  mangoTree.harverst();
-   console.log(`[Year ${mangoTree.umur} Report] Height = ${mangoTree.tinggi} | Fruits harvested = ${mangoTree.kumpulanBuah}`)
+   mangoTree.harvest();
+   console.log(`[Year ${mangoTree.umur} Report] Height = ${mangoTree.tinggi} | Fruits harvested = ${mangoTree.harvest()}`)
  } while (mangoTree.healthy != false)
 
 
@@ -88,3 +99,43 @@ class Fruit {}
 
 // Release 3
 class TreeGrove {}
+
+
+// let obj = {
+//     nama : 'Fabio',
+//     umur : '22',
+//     hobby : { hobby1 : 'Basket',
+//               hobby2 : 'Game',
+//               hobby3 : 'Javascript'
+//             },
+//     dream : 'Change the world',
+//     alamat : 'Sultan Hasanuddin',
+//
+//     jalan : function(tempat) {
+//       return 'Mobil Jalan ke ' + tempat
+//     },
+//
+//     hidupkan : function(status) {
+//       return 'Mobil Hidup hingga ' + status
+//     }
+// };
+
+
+// let arr = []; array baru
+// console.log(obj.nama); mengakses obj dengan property nama dan value fabio
+// obj.nama = 'ivon'; mengganti value dari property nama dengan ivon
+// console.log(obj.nama); mengakses objek dengan property nama dan value yang sudah berubah menjadi ivon
+// console.log(obj.umur); mengakses obj dengan umur dan valuenya (22)
+// obj.umur = '23'; mengganti value dari property umur menjadi 23
+// console.log(obj.umur) mengakses objek dengan property umur dan value yang sudah berubah menjadi 23
+// arr.push(obj); mencoba untuk push semua objek di atas ke dalam array [{}];
+// console.log(arr); mencetak array yang di dalamnya sudah ada object
+// console.log(obj); mengakses objek obj dengan semua property dan nilai yang ada di dalam objek tersebut
+// console.log(arr[0].umur); mengakses array of object yang sudah ada ,akses untuk nilai di array ke 0 dan property umur
+// console.log(obj.jalan('Amerika')); mengakses objek dengan method/function dengan satu parameter isinya 'Amerika'
+// console.log(obj.hidupkan('Meledak')); mengakses objek dengan method hidupkan dengan satu parameter isinya 'Meledak'
+// console.log(obj["jalan"]('Australia')); mengakses objek dengan cara bracket notation dan satu parameter dengan isi Australia
+// console.log(obj["hidupkan"]('Tidak Meledak')) mengakses objek dengan cara bracket notation dan satu parameter dengan isi 'Tidak Meledak'
+// console.log(obj.hobby.hobby1); cara mengakses nested array
+// console.log(obj.hobby.hobby2); cara mengakses nested array
+// console.log(obj.hobby.hobby3); cara mengakses nested array
