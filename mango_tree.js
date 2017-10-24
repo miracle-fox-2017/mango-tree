fe2581483c30a6bec7e9d9bc87118eb9c11fc21c
Ayunda <ayundahamzah@gmail.com>
2017-10-24 22:55:17 +0700

    release

--- mango_tree.js
+++ mango_tree.js
@@ -8,8 +8,9 @@ class MangoTree {
   constructor() {
     this.umur=0;
     this.tinggi=0;
-    this.Maxbuah=50;
-    this.jumlahBuah=0;
+    this.Maxbuah=10;
+    this.jumlahBuah = []
+    this.Buah=0;
     this.buahPetik=0;
     this.isHealty=true;
     this.umurMax=20;
@@ -51,16 +52,33 @@ class MangoTree {
 
   // Produce some mangoes
   produceMangoes() {
-    this.jumlahBuah += Math.floor(Math.random()*this.Maxbuah)
+    // this.jumlahBuah = [];
+    this.Buah = Math.floor(Math.random() * this.Maxbuah);
+    for (let i = 0; i < this.Buah; i++) {
+      let manggo = new Mango();
+      this.jumlahBuah.push(manggo.getQuality());
+    }
+
   }
 
   // Get some fruits
   harvest() {
-      this.buahPetik += Math.floor(Math.random()*this.jumlahBuah)
+    let countBad = 0;
+    let countGood = 0;
+    for(let i = 0;i < this.jumlahBuah.length;i++){
+      // console.log('ini jumlah buah'+this.jumlahBuah);
+      if (this.jumlahBuah[i]==='Good'){
+        countGood++
+      }else{
+        countBad++
+      }
     }
-
-
+    return `Good ${countGood} | Bad ${countBad}`;
+  }
 }
+
+
+
 
 class Mango extends MangoTree{
   // Produce a mango
@@ -70,11 +88,11 @@ class Mango extends MangoTree{
     this.buahGakSehat=0
   }
   getQuality(){
-    let jumlah = this.buahSehat+this.buahGakSehat
-    if(jumlah<this.buahPetik){
-      this.buahSehat = Math.floor(Math.random()*this.buahPetik)
-      this.buahGakSehat = jumlah - this.buahSehat
-
+    let random = Math.floor(Math.random()*2);
+    if (random==1){
+      return 'Bad';
+    }else{
+      return 'Good';
     }
 
   }
@@ -87,7 +105,7 @@ class Mango extends MangoTree{
      mangoTree.grow();
      mangoTree.produceMangoes();
      mangoTree.harvest();
-     console.log(`[Year ${mangoTree.getAge()} Report] Height = ${mangoTree.getHeight()} | Fruits harvested = ${mangoTree.getHarvest()}`)
+     console.log(`[Year ${mangoTree.getAge()} Report] Height = ${mangoTree.getHeight()} | Fruits harvested = ${mangoTree.harvest()}`)
    } while (mangoTree.isHealthy != false)
 
 
